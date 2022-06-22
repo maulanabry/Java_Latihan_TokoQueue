240afc737e805c06863be7576e6777a8f606c607
maulanabry <97007181+maulanabry@users.noreply.github.com>
2022-06-23 06:31:55 +0700

    Initial commit

---
+++ src/LatihanFinalProject/cBarang.java
@@ -0,0 +1,16 @@
+package LatihanFinalProject;
+public class cBarang {
+    String nama;
+    int harga;
+
+    public cBarang(String n, int h) {
+        nama = n;
+        harga = h;
+    }
+    public String getNama(){
+        return nama;
+    }
+    public int getHarga(){
+        return harga;
+    }
+}
